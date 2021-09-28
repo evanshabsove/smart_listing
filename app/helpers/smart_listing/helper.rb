4a3fab2b1cf354d0c6b2faa3b4e8877d79496134
Ben Eggett <beneggett@gmail.com>
2021-09-28 15:41:10 -0600

    Updates pagination for Ruby 3 syntax to work

--- app/helpers/smart_listing/helper.rb
+++ app/helpers/smart_listing/helper.rb
@@ -46,7 +46,11 @@ module SmartListing
 
       def paginate options = {}
         if @smart_listing.collection.respond_to? :current_page
-          @template.paginate @smart_listing.collection, {:remote => @smart_listing.remote?, :param_name => @smart_listing.param_name(:page)}.merge(@smart_listing.kaminari_options)
+          if Gem::Version.new(RUBY_VERSION) > Gem::Version.new('3.0')
+            @template.paginate @smart_listing.collection, **{:remote => @smart_listing.remote?, :param_name => @smart_listing.param_name(:page)}.merge(@smart_listing.kaminari_options)
+          else
+            @template.paginate @smart_listing.collection, {:remote => @smart_listing.remote?, :param_name => @smart_listing.param_name(:page)}.merge(@smart_listing.kaminari_options)
+          end		
         end
       end
 
